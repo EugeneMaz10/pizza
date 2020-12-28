c16f4567a9dcb60c8b17b62be235ac82674d0bc7
olesigilai <ole_sigilai@yahoo.com>
2020-12-28 23:32:10 +0300

    styling and js content added

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var Pizza ={
+
+pizzaSize : ["Regular", "Medium", "Large"],
+PizzaCrust : ["Thin crust", "Thick crust", "Deep crust", "Stuffed crust"],
+PizzaTopping : ["Onion", "Mushroom", "bacon", "black olives"]
+
+};
