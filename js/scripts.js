97d35d46f633f906e36cc61053d0cd5a4ad7f9c1
olesigilai <ole_sigilai@yahoo.com>
2020-12-31 00:05:53 +0300

    js content is added

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
-var Pizza ={
-
-pizzaSize : ["Regular", "Medium", "Large"],
-PizzaCrust : ["Thin crust", "Thick crust", "Deep crust", "Stuffed crust"],
-PizzaTopping : ["Onion", "Mushroom", "bacon", "black olives"]
-
-};
+function Pizza(pizzaFlavour,pizzaSize,pizzaCrust,pizzaTopping) {
+this.pizzaFlavour=pizzaFlavour;
+this.pizzaSize= pizzaSize;
+this.pizzaCrust=pizzaCrust;
+this.pizzaTopping=pizzaTopping;
+}
+var pizzaPrice= [900,600,400]
+function Total(pizzaPrice,pizzaNumber){
+    this.pizzaPrice=pizzaPrice;
+    this.pizzaNumber = pizzaNumber;
+}
+function myPizza(){
+    var pizzaFlavour= document.getElementById("Flavour").value;
+    alert(pizzaFlavour);
+    
+    var pizzaSize=parseInt( document.getElementById("Size").value);
+    alert(pizzaSize);
+    var pizzaCrust = parseInt(document.getElementById("Crust").value);
+    alert(pizzaCrust);
+    var pizzaTopping = parseInt( document.getElementById("Topping").value);
+    alert(pizzaTopping);
+    var number = parseInt( document.getElementById("number").value);
+    alert(number);
+    var order = new Pizza(pizzaFlavour,pizzaSize,pizzaCrust,pizzaTopping)
+    alert(order);
+}
